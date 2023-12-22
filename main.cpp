18e9d675a7d319e79411b2fe9445a4f5a69bdccd
igor-oliveirasilvestre <igor.oliveirasilvestre95@gmail.com>
2023-12-22 09:26:20 -0300

    Comment
    Descriptions of functions and blocks of code added

--- main.cpp
+++ main.cpp
@@ -8,13 +8,13 @@
 // Function to calculate roll angle 'aerospace rotation sequence' Eq.25
 double calculateRoll(double accel_x, double accel_y, double accel_z) {
     return atan2(accel_y, accel_z);
-    //return atan2(accel_y, accel_z)*180/M_PI; // convert radian to degree if wanted !change it to degree on line 68!
+    //return atan2(accel_y, accel_z)*180/M_PI; // convert radian to degree if wanted !change it to degree on line 76!
 }
 
 // Function to calculate pitch angle 'aerospace rotation sequence' Eq.26
 double calculatePitch(double accel_x, double accel_y, double accel_z) {
     return atan2(-accel_x, sqrt(accel_y * accel_y + accel_z * accel_z));
-    //return atan2(-accel_x, sqrt(accel_y * accel_y + accel_z * accel_z))*180/M_PI; //return atan2(accel_y, accel_z)*180/M_PI; // convert radian to degree if wanted  !change it to degree on line 68!
+    //return atan2(-accel_x, sqrt(accel_y * accel_y + accel_z * accel_z))*180/M_PI; //return atan2(accel_y, accel_z)*180/M_PI; // convert radian to degree if wanted  !change it to degree on line 76!
 }
 
 
@@ -28,6 +28,14 @@ struct SensorData {
     double accel_z;
 };
 
+/**
+ * Reads sensor data from a log file, calculates roll and pitch angles, and writes the results to an output file.
+ *
+ * @return 0 if the function executes successfully
+ *
+ * @throws Error opening input file if there was an error opening the input file
+ * @throws Error opening output file if there was an error opening the output file
+ */
 int main() {
     std::ifstream inputFile("attitude_exam.log");
     std::ofstream outputFile("attitude_result.txt");
@@ -41,13 +49,13 @@ int main() {
     std::cout << "Error opening output file." << std::endl;
     return 1;
 }
-
+    // Read sensor data from the input file
     std::vector<SensorData> sensorDataList;
     std::string line;
     while (std::getline(inputFile, line)) {
         std::istringstream iss(line);
         SensorData data;
-
+        
         char delimiter;
         if (!(iss >> data.time_stamp_ms >> delimiter >> data.accel_x >> delimiter >>
               data.accel_y >> delimiter >> data.accel_z)) {
@@ -64,9 +72,9 @@ int main() {
         double roll = calculateRoll(data.accel_x, data.accel_y, data.accel_z);
         double pitch = calculatePitch(data.accel_x, data.accel_y, data.accel_z);
 
-        // Write results to the output file
+        // Write results to the output file, if changed to degrees on functions calculateRoll and calculatePitch change it here as well
         outputFile << "Timestamp: " << data.time_stamp_ms << " ms, Roll: " << roll << " rad, Pitch: " << pitch << " rad" << std::endl;
-
+        
         sensorDataList.push_back(data);
     }
 
@@ -74,10 +82,11 @@ int main() {
     outputFile.close();
 
     // Print parsed data as an example
+    /*
     for (const auto& data : sensorDataList) {
         std::cout << "Time: " << data.time_stamp_ms << " ms, Accel X: " << data.accel_x
                   << ", Accel Y: " << data.accel_y << ", Accel Z: " << data.accel_z << std::endl;
     }
-
+    */
     return 0;
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
-### Attitude Estimator
+# Attitude Estimator
+
+
+## How the task was performed
+
+I started by working on a c++ program that parses the data provided by the log file "attitude_exam.log". To achieve that 
